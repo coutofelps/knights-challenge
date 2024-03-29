3ca5c310bedc3bfd92ffe48258d2a3bb6b3c6960
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-03-29 15:38:42 -0300

    :green_heart: ci: create new configuration into docker-compose file to vue environment

---
+++ client/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue.js!</h1>
+    <p>Este é o seu ambiente Vue.js integrado com o Docker.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  };
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
